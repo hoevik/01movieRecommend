7ed294deec12fdd58eeebee8b5afae23d4f2e3b6
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-24 13:39:00 -0700

    added call back function

---
+++ controllers/moviesController.js
@@ -0,0 +1,15 @@
+///////// dabase /////////
+
+var db = require('../models');
+
+
+//get api/movies
+function index(req, res){
+  console.log(json);
+}
+
+
+// export public methods here
+module.exports = {
+  index: index,
+};
