b267767c8c2742efc4ec83fc3a0b534ed74bead8
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-24 14:24:44 -0700

    created schema for movies, but I think that does not hurt anyone, I guess, yet...

--- models/movie.js
+++ models/movie.js
@@ -1,7 +1,29 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
-  var movieSchema=[]; //is this where to insert api-linking
+  var movieSchema= new Schema({
+    Title: String,
+    Year: String,
+    Rated: String,
+    Released: String,
+    Runtime: String,
+    Genre: String,
+    Director: String,
+    Writer: String,
+    Actors: String,
+    Plot: String,
+    Language: String,
+    Country: String,
+    Awards: String,
+    Poster: String,
+    Metascore: String,
+    imdbRating: String,
+    imdbVotes: String,
+    imdbID: String,
+    Type: String,
+    Response: String
+
+  }); //is this where to insert api-linking
 
 
 
