eee1f56d0ec9ab51b5c13a7826a837fe2be4b2e8
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-25 11:37:54 -0700

    seeded the data, got /api/movies to work

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/movie_test");
 
-var Movie = require('./movie');
+var Movie = require('./movies');
 
 module.exports.Movie  = Movie;
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,20 @@
 //Client-side js
 
+
 $(document).ready(function() {
   console.log('app.js is loaded!');
-
-
+  $.get('/api').success(function(movies){
+    movies.forEach(function(movie){
+      renderMovie(movie);
+    });
+  });
 });
+
+
+function renderMovie(movie) {
+  console.log('rendering movie', movie);
+  var movieHtml = $('#movie-template').html();
+  var moviesTemplate = Handlebars.compile(movieHtml);
+  var html = moviesTemplate(movie);
+  $('#movies').prepend(html);
+}
