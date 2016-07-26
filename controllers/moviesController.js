a338d703151c06aea041f5d22e159583ac7eca91
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-25 19:30:34 -0700

    call back and module for destroy

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -25,13 +25,20 @@ function create(req, res) {
   });
 }
 
+function destroy(req, res) {
+  db.Movie.findOneAndRemove({ _id: req.params.movieId }, function(err, foundMovie){
+    // note you could send just send 204, but we're sending 200 and the deleted entity
+    res.json(foundAlbum);
+  });
+}
 
 
 
 // export public methods here
 module.exports = {
   index: index,
-  create: create
+  create: create,
+   destroy: destroy
   // show: show,
   // destroy: destroy,
   // update: update
