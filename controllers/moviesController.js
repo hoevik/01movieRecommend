aca5ebe61148d9e8f60a4593ddfdc84cf1059b9e
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-26 08:55:34 -0700

    colsure

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -13,11 +13,6 @@ function index(req, res) {
 //CREATE
 function create(req, res) {
   console.log('body', req.body);
-
-  // split at comma and remove and trailing space
-  // var genres = req.body.genres.split(',').map(function(item) { return item.trim(); } );
-  // req.body.genres = genres;
-
   db.Movie.create(req.body, function(err, movie) {
     if (err) { console.log('error', err); }
     console.log(movie);
@@ -35,11 +30,27 @@ function show(req, res) {
 
 
 function destroy(req, res) {
-  db.Movie.findOneAndRemove({ _id: req.params.movieId }, function(err, foundMovie){
+  db.Movie.findOneAndRemove({ _id: req.params.movieId}, function(err, foundMovie){
     // note you could send just send 204, but we're sending 200 and the deleted entity
     res.json(foundMovie);
   });
 }
+
+function update(req, res) {
+  console.log('updating with data', req.body);
+  db.Movie.findById(req.params.movieId, function(err, foundMovie) {
+    if(err) { console.log('moviesController.update error', err); }
+    foundMovie.Title = req.body.Title;
+    foundMovie.Year = req.body.Year;
+    foundMovie.Runtime = req.body.Runtime;
+    foundMovie.save(function(err, savedMovie) {
+      if(err) { console.log('saving altered movie failed'); }
+      res.json(savedMovie);
+    });
+  });
+
+}
+
 
 
 
@@ -47,8 +58,7 @@ function destroy(req, res) {
 module.exports = {
   index: index,
   create: create,
-   destroy: destroy,
-   show: show
-  // destroy: destroy,
-  // update: update
+  show: show,
+  destroy: destroy,
+  update: update
 };
